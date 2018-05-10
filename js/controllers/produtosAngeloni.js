472698b14422a0cda177408799afc83560333d5c
Marcelo Joras <marcelojoras@hotmail.com>
2018-05-10 10:42:36 -0300

    data

--- js/controllers/produtosAngeloni.js
+++ js/controllers/produtosAngeloni.js
@@ -4,6 +4,7 @@ angular.module("produtosAngeloni")
 	$scope.produtos = [];
 
 	$scope.adicionarProduto = function(produto){
+		produto.data = new Date();
 		$scope.produtos.push(angular.copy(produto)); //adicionando os dados do produto em um array
 		delete $scope.cadProduto; //resetando os campos
 		console.log($scope.produtos);
