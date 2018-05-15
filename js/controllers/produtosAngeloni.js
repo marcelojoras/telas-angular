e831061e48080557d93e673dca6ef49eb656f30e
nathalia-silva <nathalia.alice.silv@gmail.com>
2018-05-15 11:48:32 -0300

    add mensagem e remove jquery

--- js/controllers/produtosAngeloni.js
+++ js/controllers/produtosAngeloni.js
@@ -2,18 +2,27 @@ angular.module("produtosAngeloni")
 .controller("produtosAngeloniController", function($scope,  $filter, produtosAPI){
 
 	$scope.produtos = [];
-
+	
 	$scope.adicionarProduto = function(produto){
-		var linhas = $("table tbody tr");
-		var numeroLinhas = linhas.length;
-
+		var numeroLinhas = angular.element(document.querySelector("table tbody tr")).length;
+	
 		produto.pedido = numeroLinhas + 1;
 		produto.data = new Date();
 		$scope.produtos.push(angular.copy(produto)); //adicionando os dados do produto em um array
 		delete $scope.cadProduto; //resetando os campos
-
+		delete $scope.mensagem; //resetando texto da mensagem
+		angular.element(document.querySelector(".mensagem")).removeClass("mensagem");
+		
 		/*produtosAPI.setProdutos($scope.produtos).then(function(promisse){
 
 		});*/
 	};
+
+	/* Mensagem caso não possua pedido */
+	var qtdLinhas = angular.element(document.querySelector("table tbody tr")).length;
+
+	if(qtdLinhas < 1){
+		$scope.mensagem ="Nenhum Pedido Cadastrado.";
+	}
+
 });
